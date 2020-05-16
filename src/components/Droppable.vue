1a5dafd762a906b86e3bc5e91ca5cf767457de38
Yanhao Li <hi@yanhaoli.com>
2020-05-16 07:19:00 -0400

    feat: add transition

--- src/components/Droppable.vue
+++ src/components/Droppable.vue
@@ -1,11 +1,13 @@
 <template>
   <draggable :list="fields" group="fields" class="droppable">
-    <Field
-      v-for="field in fields"
-      :data="field"
-      :key="field.id"
-      @delete="deleteField"
-    />
+    <transition-group name="field-list" class="droppable">
+      <Field
+        v-for="field in fields"
+        :data="field"
+        :key="field.id"
+        @delete="deleteField"
+      />
+    </transition-group>
   </draggable>
 </template>
 
@@ -39,8 +41,22 @@ export default {
 
 <style lang="scss" scoped>
 .droppable {
+  display: block;
   height: 100%;
   min-height: 100px;
   width: 100%;
+}
+
+.field-list-move {
+  transition: transform 0.5s;
+}
+
+.field-list-leave-active {
+  transition: all 1s;
+}
+
+.field-list-leave-to {
+  opacity: 0;
+  transform: translateX(100px);
 }
 </style>
